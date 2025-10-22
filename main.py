2ae57362fa192cae2d877c7e766547e39a8b58b3
Саня <halekser@mail.ru>
2025-10-23 02:49:30 +0300

    Add /echo endpoint1

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello, Serverless! 🚀\n", 200, {'Content-Type': 'text/plain'}
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json()
+    return jsonify({
+        "status": "received",
+        "you_sent": data,
+        "length": len(str(data)) if data else 0
+    })
